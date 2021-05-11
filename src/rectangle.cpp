9233beda7235e80228a2c21e79bcc42483c5fd52
cheyenneayyoub <cayyo001@ucr.edu>
2021-05-10 22:22:34 -0700

    Implimented getters, constructor, and perimiter

---
+++ src/rectangle.cpp
@@ -0,0 +1,29 @@
+nclude "../header/rectangle.hpp"
+
+Rectangle::Rectangle() : width(0), height(0) {}
+
+Rectangle::Rectangle(int w, int h) : width(w), height(h) {}
+
+void Rectangle::set_width(int w) { 
+	this->width = w; 
+}
+
+void Rectangle::set_height(int h) { 
+	this->height = h; 
+}
+
+int Rectangle::get_width() { 
+	return width; 
+}
+
+int Rectangle::get_height() { 
+	return height; 
+}
+
+int Rectangle::perimeter() {
+	return 2 * (width+height);
+}
+
+int Rectangle::area(){
+	return this->width * this->height;
+} 
