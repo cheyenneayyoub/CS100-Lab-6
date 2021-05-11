cb213330ab46efef430aa9440b774091dce2c6ec
Shwena Kak <74133408+shwenakak@users.noreply.github.com>
2021-05-10 23:43:11 -0700

    Update main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "gtesr/gtest.h"
+#include "../tests/test.cpp"
+#include "../header/rectangle.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+}
