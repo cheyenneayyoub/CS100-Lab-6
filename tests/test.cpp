15f4cf37cca21d8c01b7997220d8e56f91c8b1b8
cheyenneayyoub <cayyo001@ucr.edu>
2021-05-10 23:17:56 -0700

    Added more tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -19,7 +19,47 @@ TEST(ConstructorTest, Negative){
         EXPECT_EQ(r.get_height(), -3);
 }
 
+TEST(AreaTest, Positive){
+	Rectangle r;
+	r.set_width(2);
+	r.set_height(3);
+	EXPECT_EQ(6, r.area());
+}
 
+TEST(AreaTest, Negative){
+        Rectangle r;
+        r.set_width(-2);
+        r.set_height(3);
+        EXPECT_THROW(r.area(), invalid_argument);
+}
+
+TEST(AreaTest, Zero){
+	Rectangle r;
+	r.set_width(0);
+	r.set_height(0);
+	EXPECT_EQ(0, r.area());
+}
+
+TEST(PerimTest, Positive){
+	Rectangle r;
+	r.set_width(2);
+	r.set_height(3);
+	EXPECT_EQ(10, r.perimiter());
+}
+
+TEST(PerimTest, Negative){
+	Rectangle r;
+	r.set_width(2);
+	r.set_height(-3);
+	EXPECT_THROW(r.perimiter(), invalid_argument);
+}
+
+TEST(PerimTest, Zero){
+	Rectangle r;
+	r.set_width(0);
+	r.set_height(0);
+	EXPECT_EQ(0, r.perimiter());
+}
 
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
